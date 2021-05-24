e3b83b1a061e3dbb1fa349ef518d0ef395ff5130
Sckad00sh <83740112+Sckad00sh@users.noreply.github.com>
2021-05-23 20:37:42 -0400

    Almost done
    Finished all but final requirement. Code takes payments and calculates change. Last step is making the order

--- CoffeeMachine/Main.py
+++ CoffeeMachine/Main.py
@@ -5,15 +5,46 @@ userSelection = input("What would you like? (espresso/latte/cappuccino):")
 water = CoffeMachine.resources['water']
 milk = CoffeMachine.resources['milk']
 coffee = CoffeMachine.resources['coffee']
-money = 0
+money = CoffeMachine.resources['money']
 
 is_on = True
-def generatereport():
-    print(f"Water: {water}ml \nMilk: {milk}ml \nCoffee: {coffee} \nMoney: {money}")
 
-def checkresources(selection):
-    selectionwater = CoffeMachine.MENU[selection]['ingredients']['water']
-    #if CoffeMachine.resources['']
+
+def generatereport():
+    print(f"Water: {water}ml \nMilk: {milk}ml \nCoffee: {coffee}g \nMoney: ${money}")
+
+
+def check_resources(ingredients):
+    # Todo: 1. get ingredients for selection
+    for item in ingredients:
+        if ingredients[item] >= CoffeMachine.resources[item]:
+            print(f'Not enough {item}')
+            return False
+    return True
+
+def process_coins(cost):
+    print (f'Insert ${cost}')
+    quarters = input('Insert quarters: ')
+    dimes = input('Insert dimes: ')
+    nickles = input('Insert nickles: ')
+    pennies = input('Insert pennies: ')
+
+    total = (0.25 * float(quarters)) + (0.10 * float(dimes)) + (0.05 * float(nickles)) + (0.01 * float(pennies))
+    money == money + total
+
+    if total == cost:
+        return True
+    if total > cost:
+        print(f'Total Inserted ${total}')
+        print('Dispensing change')
+        change = total - cost
+        print(f'Your change is ${change}')
+        money == money - change
+        return True
+    else:
+        print(f'Sorry that\'s not enough money. Money refunded')
+        return False
+
 
 while is_on:
     if userSelection == "off":
@@ -23,15 +54,9 @@ while is_on:
         print("Generating Report...")
         generatereport()
         is_on = False
-    elif userSelection == "espresso":
-        checkresources(userSelection)
-        print()
-        is_on = False
-    elif userSelection == "latte":
-        checkresources(userSelection)
-        print()
-        is_on = False
-    elif userSelection == "cappuccino":
-        checkresources(userSelection)
-        print()
+    else:
+        choice = CoffeMachine.MENU[userSelection]
+        if check_resources(choice['ingredients']):
+            process_coins(choice['cost'])
+        generatereport()
         is_on = False
